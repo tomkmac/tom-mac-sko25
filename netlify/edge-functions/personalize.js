465e81b61b54ac952f88c0a8f8aca30656d3bf3c
netlifyjoe <142803939+netlifyjoe@users.noreply.github.com>
2024-12-22 10:35:58 -0500

    Update personalize.js

--- netlify/edge-functions/personalize.js
+++ netlify/edge-functions/personalize.js
@@ -1,5 +1,4 @@
 import { HTMLRewriter } from "https://ghuc.cc/worker-tools/html-rewriter/index.ts";
-import { fetchConnectData } from "../../utilities/connect.js";
 
 export default async (request, context) => {
     const url = new URL(request.url);
@@ -19,42 +18,7 @@ export default async (request, context) => {
             })
 
         return rewriter.transform(response);
-    } else if (url.searchParams.get("method") === "connect") {
-
-        const data = await fetchConnectData();
-        console.log(data);
-
-        const rewriter = new HTMLRewriter()
-            .on("#netlify-title", {
-                element: (element) => {
-                    if (location) {
-                        element.setInnerContent(`${data.title}?`);
-                        element.setAttribute("data-sb-object-id", data._objectId);
-                        element.setAttribute("data-sb-field-path", ".title");
-                    } else {
-                        element.setInnerContent(`Hello from Netlify!`);
-                    }
-                },
-            })
-
-        return rewriter.transform(response);
-    } else if (url.searchParams.get("method") === "imgreplace") {
-        const rewriter = new HTMLRewriter()
-            .on(".netlify-img", {
-                element: (element) => {
-                    const width = element.getAttribute("width") ?? 800;
-                    const height = element.getAttribute("height") ?? 600;
-                    element.setAttribute(
-                        "src",
-                        `https://placehold.co/${width}x${height}?text=Hello+Belk`
-                    );
-                    element.setAttribute("alt", "A random cat");
-                }
-            })
-
-        return rewriter.transform(response);
     }
-
 
     return response;
 };
